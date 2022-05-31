55cf72fbde6459023cedef4950881249c335cf33
esrasen9 <esrasen860@gmail.com>
2022-05-31 15:11:49 +0300

    locating elements with contains

--- project-1/cypress/integration/locators.spec.js
+++ project-1/cypress/integration/locators.spec.js
@@ -35,4 +35,22 @@ describe("Locators", () => {
     */
     cy.getByTestId("btn-id-1");
   });
+
+  it("Locating elements with contains", () => {
+    // Contains get only one element
+
+    // Get element by text
+    cy.contains("Unique Text");
+    cy.contains("Not Unique Text");
+
+    // Combine with selector
+    cy.contains("[type='submit']", "Not Unique Text");
+
+    // Combine with tag name
+    cy.contains("h3", "Contains");
+    cy.contains("form", "Not Unique Text");
+
+    // Combine with get
+    cy.get("button.btn-1").contains("Button in Form");
+  });
 });
